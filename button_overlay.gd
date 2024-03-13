ada496cfddeca9a22de5284225334f3122fc79ac
paradoxniteowl <148103497+paradoxniteowl@users.noreply.github.com>
2024-03-13 11:13:20 -0500

    more code

--- button_overlay.gd
+++ button_overlay.gd
@@ -1,13 +1,18 @@
 extends CanvasLayer
 
-@onready var current_scene = get_tree().get_current_scene
-@onready var normalwld = load("res://normal_world.tscn")
-@onready var blklitewld = load("res://blacklight_world.tscn")
+@onready var current_scene = get_tree().current_scene
+@onready var normalwld = $normal_world
+@onready var normalwld_player = $normal_world/TileMap/player
+@onready var blklitewld = $blacklight_world
+@onready var blklitewld_player = $blacklight_world/TileMap/player
+
+func _ready():
+	print(current_scene)
 
 func _on_blacklight_button_pressed():
-	if current_scene == load("res://normal_world.tscn"):
+	if current_scene == normalwld:
 		get_tree().change_scene_to_file("res://blacklight_world.tscn")
 		BlacklightWorld.player.position = NormalWorld.player.position
 	else:
 		get_tree().change_scene_to_file("res://normal_world.tscn")
-		NormalWorld.player.position = BlacklightWorld.player.position
+		normalwld_player.global_position = blklitewld_player.global_position
