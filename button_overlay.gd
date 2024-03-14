86d65d250635c1bcd05cf9c81de15995b45e06e8
paradoxniteowl <148103497+paradoxniteowl@users.noreply.github.com>
2024-03-14 11:13:16 -0500

    kind of not really working but close?

--- button_overlay.gd
+++ button_overlay.gd
@@ -1,18 +1,23 @@
 extends CanvasLayer
 
 @onready var current_scene = get_tree().current_scene
-@onready var normalwld = $normal_world
+@onready var normalwld = preload("res://normal_world.tscn")
 @onready var normalwld_player = $normal_world/TileMap/player
 @onready var blklitewld = $blacklight_world
 @onready var blklitewld_player = $blacklight_world/TileMap/player
 
 func _ready():
-	print(current_scene)
+	print(normalwld)
+
 
 func _on_blacklight_button_pressed():
-	if current_scene == normalwld:
-		get_tree().change_scene_to_file("res://blacklight_world.tscn")
-		BlacklightWorld.player.position = NormalWorld.player.position
-	else:
-		get_tree().change_scene_to_file("res://normal_world.tscn")
-		normalwld_player.global_position = blklitewld_player.global_position
+	
+
+#	if current_scene == normalwld:
+#		print("switched")
+#		get_tree().change_scene_to_file("res://blacklight_world.tscn")
+#		#blklitewld_player.global_position = normalwld_player.global_position
+#	else:
+#		print("unswitched")
+#		get_tree().change_scene_to_file("res://normal_world.tscn")
+		#normalwld_player.global_position = blklitewld_player.global_position
--- normal_world.gd
+++ normal_world.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
 @onready var player_position = $TileMap/player
+const TILE_TEXTURES = ["res://normal_set.tres", "res://blacklight_set.tres"]
+var tile = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,3 +12,9 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_change_light_pressed():
+	tile = (tile + 1)%2
+	var texture = load(TILE_TEXTURES[tile])
+	$TileMap.tile_set.get_source(1).texture = texture
